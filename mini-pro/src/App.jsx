1d8861de0ba1191e20aef94188da63279c97d9f0
harshita <harshita@logicrays.com>
2024-08-30 15:10:15 +0530

    30aug

---
+++ mini-pro/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./features/Header"
+import Products from "./features/Products"
+
+
+function App() {
+  return (
+  <>
+    <Header/>
+    <Products/>
+  </>
+  )
+}
+
+export default App
---
+++ mini-pro/src/assets/productlist.js
@@ -0,0 +1,17 @@
+import Image1 from '/src/assets/images/a.jpg'
+import Image2 from '/src/assets/images/b.jpg'
+import Image3 from '/src/assets/images/car1.jpg'
+import Image4 from '/src/assets/images/chair.jpg'
+import Image5 from '/src/assets/images/chair3.webp'
+import Image6 from '/src/assets/images/c.jpeg'
+import Image7 from '/src/assets/images/findcar.jpg'
+
+export let productlist = [
+    {id:"1", name:"product1",price:2000,category:'grocery',brand:'brand1',image:Image1,stock:10},
+    {id:"2", name:"product2",price:4000,category:'electronics',brand:'brand2',image:Image2,stock:100},
+    {id:"3", name:"product3",price:6000,category:'accessories',brand:'brand3',image:Image4,stock:20},
+    {id:"4", name:"product4",price:7000,category:'fashion',brand:'brand4',image:Image3,stock:1},
+    {id:"5",name:"product5",price:9000,category:'electronics',brand:'brand1',image:Image5,stock:0},
+    {id:"6", name:"product6",price:10000,category:'grocery',brand:'brand5',image:Image6,stock:10},
+    {id:"7", name:"product7",price:2700,category:'grocery',brand:'brand3',image:Image7,stock:30}
+]   
